5c0fc77a2605ccfe8784c7af7cf27f5edebd6154
Joey Wicker <jmwicker@asu.edu>
2020-10-02 22:27:41 -0400

    Updates to pantry component.

--- src/app/pantry/pantry.component.ts
+++ src/app/pantry/pantry.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { PantryService } from '../pantry.service';
 
 import { IPantryItem } from './pantry'
-import { Title } from '@angular/platform-browser';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { IIngredient } from '../ingredients/ingredients';
+import { IMeasurement } from '../measurements/measurements';
 
 @Component({
   selector: 'app-pantry',
@@ -12,8 +14,19 @@ import { Title } from '@angular/platform-browser';
 export class PantryComponent implements OnInit {
 
   pantry: IPantryItem[] = []
+  ingredients: IIngredient[] = [];
+  measurements: IMeasurement[] = [];
+
+  pantryForm = new FormGroup({
+    ingredient: new FormControl("", Validators.required),
+    quantity: new FormControl("0", [Validators.required, Validators.min(0)]),
+    measurement: new FormControl("", Validators.required)
+  })
 
   isCollapsed = true;
+  showSaveError = false;
+
+  saveError: {};
 
   constructor(private pantryService: PantryService) { }
 
@@ -21,12 +34,47 @@ export class PantryComponent implements OnInit {
     this.pantryService.getAllPantryItems().subscribe(
       (pantry) => {
         this.pantry = pantry;
+        console.log(pantry);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 
-        console.log(this.pantry)
+    this.pantryService.getAllIngredients().subscribe(
+      (ingredients) => {
+        this.ingredients = ingredients;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    this.pantryService.getAllMeasurements().subscribe(
+      (measurements) => {
+        this.measurements = measurements;
       },
       (error) => {
         console.log(error);
       }
     )
+  }
+
+  get ingredient() { return this.pantryForm.get("ingredient"); }
+  get quantity() { return this.pantryForm.get("quantity"); }
+  get measurement() { return this.pantryForm.get("measurement"); }
+
+  save() {
+
+  }
+
+  close() {
+    this.pantryForm.reset({
+      ingredient: "",
+      quantity: "0",
+      measurement: ""
+    });
+
+    this.isCollapsed = true;
   }
 }
